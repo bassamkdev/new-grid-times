e5ad01d1324bac1e04ec7e0c8db4a03b32f7e98f
bassamkdev <bassamkazemi.dev@gmail.com>
2022-02-04 19:27:15 -0800

    corrected abstracts number of lines and added bottom border

--- src/components/SecondaryStory/SecondaryStory.js
+++ src/components/SecondaryStory/SecondaryStory.js
@@ -3,15 +3,25 @@ import styled from 'styled-components/macro';
 
 const SecondaryStory = ({ id, title, image, location, abstract }) => {
   return (
-    <a href={`/story/${id}`}>
+    <SecondryStoryLink href={`/story/${id}`}>
       <Wrapper>
         <Image alt={image.alt} src={image.src} />
         <Heading>{title}</Heading>
         <Abstract>{abstract}</Abstract>
       </Wrapper>
-    </a>
+    </SecondryStoryLink>
   );
 };
+
+const SecondryStoryLink = styled.a`
+  &:not(:last-of-type){
+    border-bottom: 1px solid var(--color-gray-300);
+    padding-Bottom: 16px;
+  }
+  &:not(:first-of-type){
+    padding-top: 16px;
+  }
+`
 
 const Wrapper = styled.article`
   display: grid;
@@ -21,6 +31,7 @@ const Wrapper = styled.article`
   gap: 4px 16px;
   grid-template-columns: 120px 1fr;
   color: var(--color-gray-900);
+
 `;
 
 const Image = styled.img`
@@ -45,6 +56,10 @@ const Abstract = styled.p`
   grid-area: abstract;
   font-size: 1rem;
   white-space: pre-wrap;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+  overflow: hidden;
 `;
 
 export default SecondaryStory;
