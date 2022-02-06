a917f4eb7df943375002cce3b71660d3c335bb2b
bassamkdev <bassamkazemi.dev@gmail.com>
2022-02-05 17:48:41 -0800

    built specialty story grid

--- src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
+++ src/components/SpecialtyStoryGrid/SpecialtyStoryGrid.js
@@ -7,6 +7,8 @@ import MarketCard from '../MarketCard';
 import SectionTitle from '../SectionTitle';
 import MiniStory from '../MiniStory';
 
+import {QUERIES} from '../../constants'
+
 const SpecialtyStoryGrid = () => {
   return (
     <Wrapper>
@@ -34,11 +36,13 @@ const SpecialtyStoryGrid = () => {
         >
           Sports
         </SectionTitle>
-        <SportsStories>
-          {SPORTS_STORIES.map((data) => (
-            <MiniStory key={data.id} {...data} />
-          ))}
-        </SportsStories>
+          <SportsStories>
+              {SPORTS_STORIES.map((data) => (
+                <MiniStoryWrapper>
+                  <MiniStory key={data.id} {...data} /> 
+                </MiniStoryWrapper>
+              ))}
+          </SportsStories>
       </SportsSection>
     </Wrapper>
   );
@@ -47,14 +51,52 @@ const SpecialtyStoryGrid = () => {
 const Wrapper = styled.div`
   display: grid;
   gap: 48px;
+  grid-template-columns: minmax(0px, auto);
+  @media ${QUERIES.tabletAndUp} {
+    gap: 64px;
+  }
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: 1fr minmax(0px, 1fr);
+    gap: 0;
+  }
 `;
 
-const MarketsSection = styled.section``;
+const MarketsSection = styled.section`
+  @media ${QUERIES.laptopAndUp} {
+    padding-right: 16px;
+    margin-right: 16px;
+    border-right: 1px solid var(--color-gray-300);
+  }
+`;
 
-const MarketCards = styled.div``;
+const MarketCards = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(min(183px, 100%), 1fr));
+  gap: 16px;
+  @media ${QUERIES.laptopAndUp} {
+    grid-template-columns: repeat(3, 1fr);
+  }
+`;
 
-const SportsSection = styled.section``;
+const SportsSection = styled.section`
+`;
 
-const SportsStories = styled.div``;
+const SportsStories = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(min(183px, 100%), 1fr));
+  gap: 16px;
+  @media ${QUERIES.tabletAndUp}{
+    display: flex;
+    grid-template-columns: revert;
+    overflow: scroll;
+  }
+`;
+
+const MiniStoryWrapper = styled.div`
+  @media ${QUERIES.tabletAndUp}{
+    min-width: 220px;
+  }
+`
+
 
 export default SpecialtyStoryGrid;
